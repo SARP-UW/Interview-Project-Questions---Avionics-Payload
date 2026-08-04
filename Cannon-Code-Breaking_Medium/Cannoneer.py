4da54258e99b2b219dcbf9d170e64cb7d167d005
Luke <verlangieriluke@gmail.com>
2026-08-03 22:59:46 -0700

    framework

---
+++ Cannon-Code-Breaking_Medium/Cannoneer.py
@@ -0,0 +1,14 @@
+from Cannon_Simulator import CannonSimulator
+import buckets # Bucket maps and codes to try
+
+# Constraints
+VMIN = 5
+VMAX = 30
+
+# Selected code and map
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
